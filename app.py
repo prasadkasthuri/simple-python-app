4d8f40343fb2444fdd6982a47cea919d76ee20e7
Prasad Kasthuri <prasad.kasthuri1@gmail.com>
2025-03-11 19:33:21 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ def hello():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run()
+    # Listen on all network interfaces (0.0.0.0) instead of localhost (127.0.0.1)
+    app.run(host='0.0.0.0', port=5000)
 
